0a8f0fbd2e6aa94227ea6d356103e14c7fdbe3dc
innieminnie <rkddlsl1001@gmail.com>
2021-04-27 13:57:04 +0900

    refactor: 박람회 소개 화면 UI 및 오토레이아웃 개선

--- Expo1900/Expo1900/Controller/MainViewController.swift
+++ Expo1900/Expo1900/Controller/MainViewController.swift
@@ -15,12 +15,12 @@ class MainViewController: UIViewController {
     @IBOutlet weak var expositionDescription: UITextView!
     @IBOutlet weak var nextButton: UIButton!
     
+    @IBOutlet weak var expositionDescriptionHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        expositionDescription.isEditable = false
-        expositionDescription.isScrollEnabled = false
-
         decodeExpositionData()
+        setUpConstraints()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,22 +43,42 @@ class MainViewController: UIViewController {
         }
     }
     
-    private func updateUIFields(with model: ExpositionInformation) {
-        expositionName.text = model.title
-        expositionPoster.image = #imageLiteral(resourceName: "poster")
+    private func setUpConstraints() {
+        expositionDescription.isEditable = false
+        expositionDescription.isScrollEnabled = false
+        expositionDescriptionHeightConstraint.constant = expositionDescription.contentSize.height
+    }
     
-        let numberFormatter = NumberFormatter()
-        numberFormatter.groupingSize = 3
-        numberFormatter.numberStyle = .decimal
-        guard let visitor = numberFormatter.string(for: model.visitors) else { return }
-        numberOfVisitor.text = "방문객 : \(visitor) 명"
-
+    private func updateUIFields(with model: ExpositionInformation) {
+        var title = model.title
+        expositionName.text = "\(title.textFormatted())"
+        expositionPoster.image = #imageLiteral(resourceName: "poster")
+        numberOfVisitor.text = "방문객 : \(model.visitors.numberFormatted()) 명"
         expositionPlace.text = "개최지 : \(model.location)"
         expositionDuration.text = "개최 기간 : \(model.duration)"
         expositionDescription.text = model.description
-
         nextButton.setTitle("한국의 출품작 보러가기", for: .normal)
     }
 }
-
-
+fileprivate extension String {
+    mutating func textFormatted() -> String {
+        if let newLineIndex = self.firstIndex(of: "(") {
+            self.insert("\n", at: newLineIndex)
+        }
+        
+        return self
+    }
+}
+fileprivate extension Int {
+    func numberFormatted() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.groupingSize = 3
+        numberFormatter.numberStyle = .decimal
+        
+        guard let visitor = numberFormatter.string(for: self) else {
+            return ""
+        }
+        
+        return visitor
+    }
+}
