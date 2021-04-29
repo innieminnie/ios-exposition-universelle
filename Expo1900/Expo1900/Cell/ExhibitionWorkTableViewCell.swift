556152ee0fe35752016ebf27a7f1d0624c1f36ba
innieminnie <rkddlsl1001@gmail.com>
2021-04-29 13:40:10 +0900

    feat: cell의 prepareForReuse 메소드 추가

--- Expo1900/Expo1900/Cell/ExhibitionWorkTableViewCell.swift
+++ Expo1900/Expo1900/Cell/ExhibitionWorkTableViewCell.swift
@@ -12,6 +12,13 @@ class ExhibitionWorkTableViewCell: UITableViewCell {
     @IBOutlet weak var workNameLabel: UILabel!
     @IBOutlet weak var workDescriptionLabel: UILabel!
     
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumbnailImageView.image = nil
+        workNameLabel.text = nil
+        workDescriptionLabel.text = nil
+    }
+    
     func setUpUI(with work: ExhibitionWork) {
         thumbnailImageView.image = work.image
         workNameLabel.text = work.name
